10d9715184e8bf1844021a7cd8a4a7f22b37915a
Antonio V <dev@tonny.org>
2017-10-21 20:30:39 -0700

    Init UI to handle elevator requests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,147 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Elevator</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="http://getbootstrap.com/dist/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://getbootstrap.com/docs/4.0/examples/narrow-jumbotron/narrow-jumbotron.css" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <header class="header clearfix">
+            <h3 class="text-muted">Elevator</h3>
+        </header>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <main role="main">
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbotron heading</h1>
+                <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+            </div>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <div class="row marketing">
+                <div class="col-2">
+                    <table class="table table-bordered table-sm text-center">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>
+                                    <i class="fa fa-building"></i>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr><td>9</td></tr>
+                            <tr><td>8</td></tr>
+                            <tr><td>7</td></tr>
+                            <tr><td>6</td></tr>
+                            <tr><td>5</td></tr>
+                            <tr><td>4</td></tr>
+                            <tr><td>3</td></tr>
+                            <tr class="table-active"><td>2</td></tr>
+                            <tr><td>1</td></tr>
+                        </tbody>
+                    </table>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col-10">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4">
+                            <table class="table table-bordered table-responsive table-sm">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center text-uppercase" colspan="2" scope="col">
+                                            Elevator
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Current Floor</th>
+                                        <td>- - -</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Direction</th>
+                                        <td>- - -</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Signal</th>
+                                        <td>- - -</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        
+                        <form class="col-sm-12 col-md-8">
+                            <h4>New Request</h4>
+
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label for="fromFloor">From</label>
+                                        <select class="form-control" id="fromFloor" name="fromFloor">
+                                            <option value="9">9</option>
+                                            <option value="8">8</option>
+                                            <option value="7">7</option>
+                                            <option value="6">6</option>
+                                            <option value="5">5</option>
+                                            <option value="4">4</option>
+                                            <option value="3">3</option>
+                                            <option value="2">2</option>
+                                            <option value="1">1</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label for="toFloor">To</label>
+                                        <select class="form-control" id="toFloor" name="toFloor">
+                                            <option value="9">9</option>
+                                            <option value="8">8</option>
+                                            <option value="7">7</option>
+                                            <option value="6">6</option>
+                                            <option value="5">5</option>
+                                            <option value="4">4</option>
+                                            <option value="3">3</option>
+                                            <option value="2">2</option>
+                                            <option value="1">1</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+
+                    <hr />
+
+                    <h4>Live Logs</h4>
+
+                    <div class="card">
+                        <div class="card-body">
+                            This is some text within a card body.
+                        </div>
+                    </div>
+
+                    <a href="#" class="btn pull-right btn-cs btn-outline-warning">Download Full Logs</a>
                 </div>
             </div>
-        </div>
-    </body>
+        </main>
+
+        <footer class="footer">
+            <p>&copy; Antonio Valencia 2017</p>
+        </footer>
+    </div> <!-- /container -->
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/e13a770015.js" crossorigin="anonymous"></script>
+</body>
 </html>
