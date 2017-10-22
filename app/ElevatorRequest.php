229f9d69b4912ec107464e772a44ab2e18efdd5c
Antonio V <dev@tonny.org>
2017-10-22 11:10:49 -0700

    Added deleted at column to elevator requests table

--- app/ElevatorRequest.php
+++ app/ElevatorRequest.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ElevatorRequest extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
     /**
      * The attributes that are mass assignable.
      *
