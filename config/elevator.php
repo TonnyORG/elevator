a7848985908d55355057caa47e75dfb7f8e1f113
Antonio V <dev@tonny.org>
2017-10-21 21:11:20 -0700

    Added elevator config file

---
+++ config/elevator.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'directions' => [
+        'down' => 'Down',
+        'stand' => 'Stand',
+        'up' => 'Up',
+    ],
+
+    'floors' => [
+        // # => Maintenance ? true : false
+        1 => true,
+        2 => false,
+        3 => true,
+        4 => false,
+        5 => true,
+        6 => true,
+        7 => true,
+        8 => true,
+        9 => true,
+    ],
+
+    'signals' => [
+        'closed' => 'Closed',
+        'open' => 'Open',
+    ],
+
+];
