9479e035009b04a5697dcbada769dd1784259dc0
Antonio V <dev@tonny.org>
2017-10-22 18:12:37 -0700

    Created elevators and elevator component

---
+++ resources/assets/js/components/elevators.component.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="row">
+        <div
+            v-for="elevator in elevators"
+            :elevator="elevator"
+            is="elevator-component"
+            class="col col-sm-6"
+        ></div>
+    </div>
+</template>
+
+<script>
+import ElevatorComponent from './elevator.component.vue';
+
+export default {
+    data() {
+        return {
+            elevators: [],
+        }
+    },
+
+    components: {
+        'elevator-component' : ElevatorComponent,
+    },
+
+    created() {
+        this.loadElevators();
+    },
+
+    methods: {
+        loadElevators() {
+            axios.get('/api/elevators')
+                .then(response => {
+                    this.elevators = response.data;
+                })
+                .catch(error => {
+                    alert(error);
+                });
+        },
+    },
+}
+</script>
